a8bae88fd8914a0a400403c21a4be27872d1bd9a
Victor Akintunde <akintoluvic@gmail.com>
2024-05-15 23:39:22 +0100

    fix auth header props

--- src/components/layouts/AuthLayout.vue
+++ src/components/layouts/AuthLayout.vue
@@ -2,15 +2,16 @@
 defineProps<{
   title: string;
   description: string;
+  widthSmall?: boolean
 }>();
 </script>
 <template>
   <div class="flex min-h-screen h-full bg-slate-800 place-items-center">
     <div
-      class="mx-auto my-auto max-w-3xl w-full pt-[71px] pb-20 px-14 bg-white rounded-2xl grid gap-6"
+      class="mx-auto my-auto max-w-[774px] w-full pt-[71px] pb-20 px-14 bg-white rounded-2xl grid gap-6"
     >
-      <div class="text-center">
-        <h1 class="text-[40px] font-semibold pb-1.5">
+      <div class="text-center" :class="{ 'max-w-[360px] w-full mx-auto': widthSmall}">
+        <h1 class="font-semibold pb-1.5" :class="[widthSmall ? 'text-[30px]' : 'text-[40px]']">
           {{ title }}
         </h1>
         <p class="text-balance text-muted-foreground">
---
+++ src/pages/auth/VerificationLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import AuthLayout from "@/components/layouts/AuthLayout.vue";
+</script>
+
+<template>
+  <AuthLayout
+    title="Check your email"
+    description="We sent a verification link to olivia@untitledui.com"
+    :widthSmall="true"
+  >
+    <div class="text-center text-sm max-w-[360px] w-full mx-auto">
+      <Button type="submit" class="w-full mt-6 mb-8"> Enter code manually </Button>
+      <RouterLink to="/signup" class="underline">Back to Sign up</RouterLink>
+    </div>
+  </AuthLayout>
+</template>

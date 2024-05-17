d0e79d55a045122b6fc04d0f15a2461a08535204
Victor Akintunde <akintoluvic@gmail.com>
2024-05-17 22:00:06 +0100

    fix pin input component

---
+++ src/components/ui/pin-input/PinInputInput.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from 'vue'
+import { PinInputInput, type PinInputInputProps, useForwardProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<PinInputInputProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+  return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <PinInputInput v-bind="forwardedProps" :class="cn('relative text-center focus:outline-none focus:ring-2 focus:ring-primary focus:relative focus:z-10 flex h-20 w-20 items-center justify-center border-y-2 border-x-2 border-l-2 border-primary text-6xl transition-all first:rounded-l-md first:border-l-2 last:rounded-r-md dark:focus:ring-slate-300 dark:border-slate-800', props.class)" />
+</template>

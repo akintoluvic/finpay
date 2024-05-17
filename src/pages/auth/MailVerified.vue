ac1c3fb07c52f6ac525c93302d597b4840d2a8be
Victor Akintunde <akintoluvic@gmail.com>
2024-05-17 22:11:32 +0100

    fix auth layout

--- src/pages/auth/MailVerified.vue
+++ src/pages/auth/MailVerified.vue
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import AuthLayout from "@/components/layouts/AuthLayout.vue";
+import AuthLayout from "@/components/auth/layout/AuthLayout.vue";
 import { ArrowLeftIcon } from '@radix-icons/vue'
 </script>
 

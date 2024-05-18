20463457a0417171ad7235edc11358c04d4d2776
Victor Akintunde <akintoluvic@gmail.com>
2024-05-18 20:56:16 +0100

    fix sidebar nav icons

---
+++ src/components/icons/TransactionsWhiteIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 17.0464H20M20 17.0464L16 13.0464M20 17.0464L16 21.0464M20 7.04639H4M4 7.04639L8 3.04639M4 7.04639L8 11.0464" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</template>

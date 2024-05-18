c7f3d6f06fb35a79ab84a5ac6e225c9f24b6a27c
Victor Akintunde <akintoluvic@gmail.com>
2024-05-18 20:45:17 +0100

    fix dasboard header layout

--- src/pages/dashboard/layout/DashboardLayout.vue
+++ src/pages/dashboard/layout/DashboardLayout.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
-import LogoWhite from "@/components/icons/LogoWhite.vue";
+import {
+  NotificationIcon,
+  LogoWhite,
+  SendIcon,
+  FundIcon,
+  ConvertIcon,
+  InvoicesIcon,
+  DownIcon
+
+} from "@/components/icons";
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { ref, watch } from "vue";
 import { useRoute } from 'vue-router'
 
@@ -9,6 +27,7 @@ console.log('route', route.path)
 
 defineProps<{
   title: string;
+  greeting?: string
 }>();
 
 
@@ -38,7 +57,7 @@ watch(() => route.path, (newId, oldId) => {
 </script>
 <template>
   <main class="flex bg-background place-items-center">
-    <div class="w-[280px] bg-primary h-screen rounded-r-xl">
+    <div class="w-[280px] bg-primary h-screen rounded-r-2xl">
       <LogoWhite class="mx-auto mt-[42px] mb-10" />
       <div class="px-4 grid gap-y-6 pt-12 border-t border-primary-light">
         <RouterLink 
@@ -57,9 +76,53 @@ watch(() => route.path, (newId, oldId) => {
       </RouterLink>
       </div>
     </div>
-    <div class="flex-1 h-screen overflow-y-scroll">
-      <div class="absolute z-50 bg-white h-[113px] w-full border-b pl-16 pr-28 pt-10">
-        <h1 class="text-3xl font-semibold">{{ title }}</h1>
+    <div class="flex-1 h-screen overflow-y-scroll text-muted">
+      <div class="absolute z-50 flex justify-between bg-white h-[113px] w-[calc(100%-280px)] border-b pl-16 pr-28 pt-8">
+        
+        <div>
+          <h1 class="text-3xl font-semibold">{{ title }}</h1>
+          <p :v-if="greeting" class="text-sm text-muted-foreground">{{ greeting }}</p>
+        </div>
+        <div class="flex gap-x-6">
+          <DropdownMenu>
+            <DropdownMenuTrigger class="flex items-center gap-x-2 self-start px-3.5 py-2.5 border border-border/60 rounded-lg font-semibold">
+              <DownIcon />
+              <span class="text-muted">Quick Actions</span>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent class="w-40">
+              <DropdownMenuLabel>
+                <div class="flex items-center gap-2">
+                  <SendIcon />
+                  <span>Send Money</span>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <div class="flex items-center gap-2">
+                  <FundIcon />
+                  <span>Fund Wallet</span>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <div class="flex items-center gap-2">
+                  <ConvertIcon />
+                  <span>Convert Funds</span>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <div class="flex items-center gap-2">
+                  <InvoicesIcon />
+                  <span>Create new invoice</span>
+                </div>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Button class="rounded-full" variant="outline" size="icon">
+            <NotificationIcon class="w-5 h-5" />
+          </Button>
+        </div>
       </div>
       <div class="pl-16 pr-28 mt-[161px]">
         <slot />

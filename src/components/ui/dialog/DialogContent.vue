eaec9da4fc08b251392f86d8e061a9ec8d62ccfd
Victor Akintunde <akintoluvic@gmail.com>
2024-05-22 11:45:35 +0100

    fix show dialog close

--- src/components/ui/dialog/DialogContent.vue
+++ src/components/ui/dialog/DialogContent.vue
@@ -12,7 +12,10 @@ import {
 import { Cross2Icon } from '@radix-icons/vue'
 import { cn } from '@/lib/utils'
 
-const props = defineProps<DialogContentProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<DialogContentProps & { 
+  class?: HTMLAttributes['class']
+  showCloseButton?: boolean
+}>()
 const emits = defineEmits<DialogContentEmits>()
 
 const delegatedProps = computed(() => {
@@ -40,6 +43,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
       <slot />
 
       <DialogClose
+        v-if="showCloseButton"
         class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 data-[state=open]:text-slate-500 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800 dark:data-[state=open]:text-slate-400"
       >
         <Cross2Icon class="w-4 h-4" />

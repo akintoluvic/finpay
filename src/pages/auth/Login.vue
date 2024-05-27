72a9658e809cfb638bbadfd386a919bdbb318363
Victor Akintunde <akintoluvic@gmail.com>
2024-05-27 11:12:35 +0100

    fix login nav

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -3,13 +3,14 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import AuthLayout from "@/pages/auth/layout/AuthLayout.vue";
+import { ArrowRightIcon } from "@radix-icons/vue";
 </script>
 
 <template>
   <AuthLayout
     title="Login to your account"
     description="Securely login tp your FinPay accounnt"
-    :showLogo="true"
+    showLogo
   >
     <div class="grid mt-10">
       <div class="grid gap-2 mb-8">
@@ -30,7 +31,10 @@ import AuthLayout from "@/pages/auth/layout/AuthLayout.vue";
       </div>
       <div class="mt-2 text-center text-sm">
         <Button type="submit" class="w-full mb-10"> Login </Button>
-        <RouterLink to="/signup" class="underline">Forgot password</RouterLink>
+        <RouterLink to="/signup" class="flex items-center justify-center gap-2 font-semibold">
+          Go to Sign up
+          <ArrowRightIcon class="h-12 stroke-gray-800" />
+        </RouterLink>
       </div>
     </div>
   </AuthLayout>

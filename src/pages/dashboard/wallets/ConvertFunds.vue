bdb2ab34bbb74331a65980afe3c5ee25bfbc3519
Victor Akintunde <akintoluvic@gmail.com>
2024-05-29 15:44:25 +0100

    fix convert funds

--- src/pages/dashboard/wallets/ConvertFunds.vue
+++ src/pages/dashboard/wallets/ConvertFunds.vue
@@ -2,8 +2,8 @@
 import Card from "@/components/Card.vue";
 import DashboardLayout from "../layout/DashboardLayout.vue";
 import { Button } from "@/components/ui/button";
-import { ConvertIcon, HomeIcon } from "@/components/icons";
-import { ArrowRightIcon, ChevronLeftIcon, ChevronRightIcon } from "@radix-icons/vue";
+import { ConvertIcon, EuroIcon, GbpIcon, HomeIcon, NgnIcon, UsdIcon } from "@/components/icons";
+import { ArrowRightIcon, ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from "@radix-icons/vue";
 import router from "@/router";
 import { ref } from "vue";
 import SuccessOrFail from "@/components/general/SuccessOrFail.vue";
@@ -11,7 +11,13 @@ import { h } from 'vue'
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import * as z from 'zod'
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import {
   FormControl,
   FormDescription,
@@ -26,15 +32,41 @@ import { toast } from '@/components/ui/toast/use-toast'
 const sendData = ref({
   step: 1, 
   type: '', 
-  sourceWallet: '',
+  convertCurrencyIndex: 0,
   amountToConvert: '', 
   amountToReceive: '',
-  destinationWallet: '',
+  receiveCurrencyIndex: 3,
 })
 
+const currencies = [
+  {
+    name: "NGN",
+    fullName: "Nigerian Naira",
+    symbol: "₦",
+    icon: NgnIcon,
+  },
+  {
+    name: "USD",
+    fullName: "US Dollar",
+    symbol: "$",
+    icon: UsdIcon,
+  },
+  {
+    name: "GBP",
+    fullName: "British Pound",
+    symbol: "£",
+    icon: GbpIcon,
+  },
+  {
+    name: "EUR",
+    fullName: "Euro",
+    symbol: "€",
+    icon: EuroIcon,
+  },
+]
+
 const formSchema = toTypedSchema(z.object({
-  occupation: z.string().min(2).max(50),
-  physicalAddress: z.coerce.number({
+  amountToConvert: z.coerce.number({
     required_error: "Field is required",
     invalid_type_error: "Values must be a number",
   }).max(5000, "Insufficient balance"),
@@ -59,13 +91,12 @@ const convertValue = (val: string) => {
   sendData.value.amountToReceive = (parseFloat(val) / 1.5).toFixed(2) + ''
 }
 const goBack = () => {
-  router && router.go(-1)
   if (sendData.value.step === 2)
     sendData.value.step -= 1
+  router && router.go(-1)
 }
 
 const nextStep = () => {
-  
 
   switch (sendData.value.step) {
     case 1:
@@ -90,11 +121,11 @@ const nextStep = () => {
   <DashboardLayout
     title="Convert Funds"
   >
-  <div class="max-w-[607px]">
+  <div class="max-w-[607px] pb-12">
     <Card v-if="sendData.step === 1" title="Convert Funds">
       <div class="flex flex-col gap-10 px-8 pb-12 pt-8">
         <form class="grid grid-cols-1 gap-y-10" @submit="onSubmit">
-              <FormField v-slot="{ componentField }" name="physicalAddress" :validate-on-blur="!isFieldDirty">
+              <FormField v-slot="{ componentField }" name="amountToConvert" :validate-on-blur="!isFieldDirty">
                 <FormItem>
                   <FormLabel>
                     Amount to Convert<span class="text-red">*</span>
@@ -102,7 +133,7 @@ const nextStep = () => {
                   <FormControl>
                     <div class="relative w-full items-center">
                       <span class="absolute start-0 inset-y-0 border-l text-primary my-2 flex items-center justify-center px-4 mr-0.5">
-                        <div class="text-2xl text-muted-foreground/50">$</div>
+                        <div class="text-2xl text-muted-foreground/50">{{ currencies[sendData.convertCurrencyIndex].symbol }}</div>
                       </span>
                       <Input
                         type="text" 
@@ -111,8 +142,38 @@ const nextStep = () => {
                         @update:modelValue="convertValue($event + '')" 
                         class="pr-10 pl-20 text-[32px] font-medium text-secondary placeholder:text-secondary/50" 
                       />
-                      <span class="absolute end-0 inset-y-0 border-l text-primary my-2 flex items-center justify-center px-4 mr-0.5">
-                        <div>Edit</div>
+                      <span class="absolute end-0 inset-y-0 text-primary my-2 flex items-center justify-center pr-2.5">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger>
+                            <Button variant="outline" class="flex items-center rounded-full gap-2 px-4 py-2.5 hover:bg-muted-background/5">
+                              <component 
+                                :is="currencies[sendData.convertCurrencyIndex].icon" 
+                                class="h-6 w-6"
+                              />
+                              <span
+                                class="text-sm font-semibold text-secondary"
+                              >{{ currencies[sendData.convertCurrencyIndex].name }} ({{ currencies[sendData.convertCurrencyIndex].symbol }})</span>
+                              <ChevronDownIcon />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent class="w-48 px-3 py-2">
+                            <span
+                              v-for="(currency, index) in currencies"
+                              :key="currency.name"
+                            >
+                            <DropdownMenuLabel
+                              @click="sendData.convertCurrencyIndex = index"
+                              class="cursor-pointer hover:bg-muted-background/15"
+                            >
+                              <div class="flex items-center gap-2 text-xs font-bold text-muted-foreground">
+                                <component :is="currency.icon" />
+                                <span>{{ currency.fullName }} ({{ currency.symbol }})</span>
+                              </div>
+                            </DropdownMenuLabel>
+                            <DropdownMenuSeparator v-if="currencies.length - 1 > index" />
+                            </span>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
                       </span>
                     </div>
                   </FormControl>
@@ -134,7 +195,7 @@ const nextStep = () => {
                   </span>
                 <div class="relative w-full items-center">
                   <span class="absolute start-0 inset-y-0 border-l text-primary my-2 flex items-center justify-center px-4 mr-0.5">
-                    <div class="text-2xl text-muted-foreground/50">£</div>
+                    <div class="text-2xl text-muted-foreground/50">{{ currencies[sendData.receiveCurrencyIndex].symbol }}</div>
                   </span>
                   <Input 
                     type="text" 
@@ -143,8 +204,39 @@ const nextStep = () => {
                     disabled
                     class="pr-10 pl-20 text-[32px] font-medium text-secondary placeholder:text-secondary/50" 
                   />
-                  <span class="absolute end-0 inset-y-0 border-l text-primary my-2 flex items-center justify-center px-4 mr-0.5">
-                    <div>Edit</div>
+                  <span class="absolute end-0 inset-y-0 text-primary my-2 flex items-center justify-center pr-2.5">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <Button variant="outline" class="flex items-center rounded-full gap-2 px-4 py-2.5 hover:bg-muted-background/5">
+                          <component 
+                            :is="currencies[sendData.receiveCurrencyIndex].icon" 
+                            class="h-6 w-6"
+                          />
+                          <span
+                            class="text-sm font-semibold text-secondary"
+                          >{{ currencies[sendData.receiveCurrencyIndex].name }} ({{ currencies[sendData.receiveCurrencyIndex].symbol }})</span>
+                          <ChevronDownIcon />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent class="w-48 px-3 py-2">
+                        <span
+                          v-for="(currency, index) in currencies"
+                          :key="currency.name"
+                        >
+                        <DropdownMenuLabel
+                          @click="sendData.receiveCurrencyIndex = index"
+                          class="cursor-pointer hover:bg-muted-background/15"
+                        >
+                          <div class="flex items-center gap-2 text-xs font-bold text-muted-foreground">
+                            <component :is="currency.icon" />
+                            <span>{{ currency.fullName }} ({{ currency.symbol }})</span>
+                          </div>
+                        </DropdownMenuLabel>
+                        <DropdownMenuSeparator v-if="currencies.length - 1 > index" />
+                        </span>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+
                   </span>
                 </div>
                 <div class="relative bg-secondary-foreground rounded-lg h-[68px] mt-2">

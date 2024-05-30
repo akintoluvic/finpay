1d0a8dad0c6e6a5ece95f9ef6b018476c3d21993
Victor Akintunde <akintoluvic@gmail.com>
2024-05-30 10:52:08 +0100

    fix datepicker

---
+++ src/components/general/DatePicker.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {
+  DateFormatter,
+  type DateValue,
+  getLocalTimeZone,
+} from '@internationalized/date'
+
+import { CalendarIcon } from '@radix-icons/vue'
+import { Calendar } from '@/components/ui/calendar'
+import { Button } from '@/components/ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+
+const df = new DateFormatter('en-US', {
+  dateStyle: 'short',
+})
+
+const firstName = defineModel('firstName')
+const value = ref<DateValue>()
+</script>
+
+<template>
+  <Popover>
+    <PopoverTrigger as-child>
+      <Button
+        variant="outline"
+        :class="cn(
+          'justify-start text-left font-normal',
+          !value && 'text-muted-foreground',
+        )"
+      >
+        
+        {{ value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date" }}
+        <CalendarIcon class="ml-auto h-4 w-4" />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent class="w-auto p-0">
+      <Calendar v-model="value" initial-focus />
+    </PopoverContent>
+  </Popover>
+</template>

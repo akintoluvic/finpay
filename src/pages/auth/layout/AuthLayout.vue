d7bc06f08f78fe3e97b5a3caa672d50cd0c19721
Victor Akintunde <akintoluvic@gmail.com>
2024-06-03 11:03:07 +0100

    fix auth mobile view

--- src/pages/auth/layout/AuthLayout.vue
+++ src/pages/auth/layout/AuthLayout.vue
@@ -9,9 +9,9 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div class="flex min-h-screen h-full bg-[#7105E9] place-items-center">
+  <div class="flex min-h-screen h-full md:bg-[#7105E9] md:px-2 place-items-center">
     <div
-      class="mx-auto my-auto max-w-[774px] w-full pt-[71px] pb-20 px-14 bg-white rounded-2xl grid gap-6"
+      class=" mx-auto md:my-auto h-auto md:max-w-[774px] w-full pt-[71px] md:pb-20 px-7 lg:px-14 bg-white md:rounded-2xl grid gap-6"
     >
 
       <div class="text-center" :class="{ 'max-w-[360px] w-full mx-auto': widthSmall}">
@@ -19,7 +19,7 @@ defineProps<{
           <slot name="image" />
         </div>
         <Logo :v-if="showLogo" class="mx-auto mb-1.5" />
-        <h1 class="font-semibold pb-1.5" :class="[widthSmall ? 'text-[30px]' : 'text-[40px]']">
+        <h1 class="font-semibold pb-1.5 leading-tight text-3xl" :class="[widthSmall ? 'md:text-[30px]' : 'md:text-[40px]']">
           {{ title }}
         </h1>
         <p class="text-balance text-muted-foreground">

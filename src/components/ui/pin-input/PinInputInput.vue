310c9d60fcf87d93067367f0d1bfdd208fee06a9
Victor Akintunde <akintoluvic@gmail.com>
2024-06-03 11:01:55 +0100

    fix pin input mobile

--- src/components/ui/pin-input/PinInputInput.vue
+++ src/components/ui/pin-input/PinInputInput.vue
@@ -14,5 +14,5 @@ const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <PinInputInput v-bind="forwardedProps" :class="cn('relative text-center focus:outline-none focus:ring-2 focus:ring-primary focus:relative focus:z-10 flex h-20 w-20 items-center justify-center border-y-2 border-x-2 border-l-2 border-primary text-6xl transition-all first:rounded-l-md first:border-l-2 last:rounded-r-md dark:focus:ring-slate-300 dark:border-slate-800', props.class)" />
+  <PinInputInput v-bind="forwardedProps" :class="cn('relative text-center focus:outline-none focus:ring-2 focus:ring-primary focus:relative focus:z-10 flex h-16 md:h-20 aspect-square items-center justify-center border-y-2 border-x-2 border-l-2 border-primary text-4xl md:text-6xl transition-all first:rounded-l-md first:border-l-2 last:rounded-r-md dark:focus:ring-slate-300 dark:border-slate-800', props.class)" />
 </template>

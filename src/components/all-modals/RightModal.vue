2bfc67c05a761124f1a2e22f0e16781262ca8d89
Victor Akintunde <akintoluvic@gmail.com>
2024-06-07 09:45:49 +0100

    fix right modal responsiveness

--- src/components/all-modals/RightModal.vue
+++ src/components/all-modals/RightModal.vue
@@ -10,11 +10,13 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 import { Badge } from '../ui/badge';
+import { LogoWhite } from '../icons';
 
 defineProps<{
   title: string
   description?: string
   step?: string
+  mobileNav?: boolean
 }>()
 </script>
 
@@ -23,16 +25,20 @@ defineProps<{
     <SheetTrigger as-child>
       <slot name="trigger" />
     </SheetTrigger>
-    <SheetContent class="w-1/2 overflow-y-scroll">
+    <SheetContent 
+      class="w-full sm:w-3/4 md:w-1/2 overflow-y-scroll"
+      :class="mobileNav ? 'bg-primary border-primary' : 'bg-white'"
+    >
       <SheetHeader>
-        <SheetTitle class="text-[30px]">
-          <span class="flex items-center gap-6">
+        <SheetTitle class="text-[30px] mt-8 lg:mt-0 text-left">
+          <LogoWhite v-if="mobileNav" class="mt-8 mb-10" />
+          <div v-else class="flex flex-col lg:flex-row lg:items-center gap-2 lg:gap-6">
             {{ title }}
-            <Badge v-if="step" variant="primary" class="py-2 px-4">Step {{ step }}/2</Badge>
-          </span>
+            <Badge v-if="step" variant="primary" class="py-2 px-4 w-[84px]">Step {{ step }}/2</Badge>
+          </div>
           
         </SheetTitle>
-        <SheetDescription v-if="description">
+        <SheetDescription v-if="description" class="text-left">
           {{ description }}
         </SheetDescription>
       </SheetHeader>
